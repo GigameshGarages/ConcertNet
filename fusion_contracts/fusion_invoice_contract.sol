81b92f03e286c80a725afdbe8b3456c5f940574b
Gokul Alex <gokulsan@users.noreply.github.com>
2020-09-01 22:51:24 +0530

    Update fusion_invoice_contract.sol

--- fusion_contracts/fusion_invoice_contract.sol
+++ fusion_contracts/fusion_invoice_contract.sol
@@ -3,15 +3,15 @@ pragma solidity ^0.4.0;
 contract Sample {
     
     /*storage*/
-    uint public invoice_index_number;
+    uint public time_series_average_number;
     
     /*function setting number to user input*/
-    function setNumber(uint _number) public {
-        invoice_index_number = _number;
+    function setTimeSeriesAverage(uint _number) public {
+        time_series_average_number = _number;
     }
     
     /*getter for the number*/
-    function getNumber() public constant returns (uint) {
-        return invoice_index_number;
+    function getTimeSeriesAverage() public constant returns (uint) {
+        return time_series_average_number;
     }
 }
