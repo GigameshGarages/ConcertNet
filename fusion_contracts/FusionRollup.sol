99e8171c98d0515b6a0ecefb5ea92cc129e5af24
Gokul Alex <gokulsan@users.noreply.github.com>
2020-09-05 23:28:32 +0530

    Create FusionRollup.sol

---
+++ fusion_contracts/FusionRollup.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.6.2;
+
+import './TokenRollup.sol'
+import './FusionToken.sol'
+import './FractalToken.sol'
+import './FiniteSwap.sol'
+import './FractalDAO.sol'
+
+
+contract FusionRollup {
+
+}
