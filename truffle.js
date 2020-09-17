03b108df0a1d4ec40e52e6106a56a7738e903fd6
dhanendras <dhanendra.chaudhary@gmail.com>
2020-09-18 02:14:16 +0530

    resolved dependencies

--- truffle.js
+++ truffle.js
@@ -1,3 +1,9 @@
+
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+
 module.exports = {
   networks: {
     development: {
